72a895a1581ca329ef927993ff8020fa4e81c1de
João Gabriel <gjacinto0707@gmail.com>
2024-04-03 16:33:09 -0300

    Migrating interface to tkinter

--- main.py
+++ main.py
@@ -1,118 +1,146 @@
-import PySimpleGUI as sg
+import tkinter as tk
+from math import pi, sin, cos, tan, sqrt, pow, factorial
 
 
-class Interface:
-    def __init__(self) -> None:
-        # Visor da calculadora
-        self.title = sg.Text('Calculadora', pad=(2, 7), key='title', expand_x=True, justification='left',
-                             colors=('#000000', '#FFFFFF'), font=('Bahnschrift', 18, 'bold'))
-        self.display = sg.Text('0', pad=(3, (0, 5)), key='display', expand_x=True, justification='right',
-                               colors=('#000000', '#C3EBEB'), font=('System', 36, 'bold'))
-
-        # Botões númericos
-        self.zero = sg.Button('0', size=(7, 2), pad=2, key='0', target='display',
-                              mouseover_colors=('#000000', '#F5F5F5'))
-        self.one = sg.Button('1', size=(7, 2), pad=2, key='1', target='display',
-                             mouseover_colors=('#000000', '#F5F5F5'))
-        self.two = sg.Button('2', size=(7, 2), pad=2, key='2', target='display',
-                             mouseover_colors=('#000000', '#F5F5F5'))
-        self.three = sg.Button('3', size=(7, 2), pad=2, key='3', target='display',
-                               mouseover_colors=('#000000', '#F5F5F5'))
-        self.four = sg.Button('4', size=(7, 2), pad=2, key='4', target='display',
-                              mouseover_colors=('#000000', '#F5F5F5'))
-        self.five = sg.Button('5', size=(7, 2), pad=2, key='5', target='display',
-                              mouseover_colors=('#000000', '#F5F5F5'))
-        self.six = sg.Button('6', size=(7, 2), pad=2, key='6', target='display',
-                             mouseover_colors=('#000000', '#F5F5F5'))
-        self.seven = sg.Button('7', size=(7, 2), pad=2, key='7', target='display',
-                               mouseover_colors=('#000000', '#F5F5F5'))
-        self.eight = sg.Button('8', size=(7, 2), pad=2, key='8', target='display',
-                               mouseover_colors=('#000000', '#F5F5F5'))
-        self.nine = sg.Button('9', size=(7, 2), pad=2, key='9', target='display',
-                              mouseover_colors=('#000000', '#F5F5F5'))
-
-        # Outros botões
-        self.clear = sg.Button('C', size=(7, 2), pad=2, key='clear', target='display',
-                               mouseover_colors=('#000000', '#F5F5F5'))
-        self.add = sg.Button('+', size=(7, 2), pad=2, key='addition', target='display',
-                             mouseover_colors=('#000000', '#F5F5F5'))
-        self.sub = sg.Button('-', size=(7, 2), pad=2, key='subtraction', target='display',
-                             mouseover_colors=('#000000', '#F5F5F5'))
-        self.mult = sg.Button('*', size=(7, 2), pad=2, key='multiplication', target='display',
-                              mouseover_colors=('#000000', '#F5F5F5'))
-        self.div = sg.Button('/', size=(7, 2), pad=2, key='division', target='display',
-                             mouseover_colors=('#000000', '#F5F5F5'))
-        self.perc = sg.Button('%', size=(7, 2), pad=2, key='percent', target='display',
-                              mouseover_colors=('#000000', '#F5F5F5'))
-        self.sqrt = sg.Button('√', size=(7, 2), pad=2, key='sq_root', target='display',
-                              mouseover_colors=('#000000', '#F5F5F5'))
-        self.sqr = sg.Button('^', size=(7, 2), pad=2, key='square', target='display',
-                             mouseover_colors=('#000000', '#F5F5F5'))
-        self.equal = sg.Button('=', size=(7, 2), pad=2, key='equal', target='display',
-                               mouseover_colors=('#000000', '#F5F5F5'))
-        self.comma = sg.Button(',', size=(7, 2), pad=2, key='comma', target='display',
-                               mouseover_colors=('#000000', '#F5F5F5'))
-
-        # Parâmetros úteis
-        # image_source = str | image_subsample = int | image_zoom = int
-
-        self.layout = self.__set_layout()
-        self.window = self.__create_window()
-
-    def __set_layout(self) -> list:
-        return [
-            [self.title],
-            [self.display],
-            [self.clear, self.perc, self.sqrt, self.sqr],
-            [self.seven, self.eight, self.nine, self.div],
-            [self.four, self.five, self.six, self.mult],
-            [self.one, self.two, self.three, self.sub],
-            [self.zero, self.comma, self.equal, self.add]
-        ]
-
-    def __create_window(self) -> sg.Window:
-        return sg.Window('Calculadora de Coisas', self.layout, resizable=True, print_event_values=True,
-                         icon='./files/icon.ico', font=('Terminal', 12, 'normal'), text_justification='center',
-                         element_justification='left', background_color='#FFFFFF', button_color=('#000000', '#FAFAFA'))
+def change_number():
+    display.insert(0, '0')
 
 
-calc = Interface()
-while True:
-    not_number = ['clear', 'addition', 'subtraction', 'multiplication',
-                  'division', 'percent', 'sq_root', 'square', 'equal', 'comma']
-    calc.event, calc.values = calc.window.read()
+# Instanciando a janela
+calc = tk.Tk()
 
-    if calc.event == sg.WIN_CLOSED:
-        break
+# Configurando a janela
+calc.title('Calculadora de Coisas')
+calc.iconbitmap('./files/icon.ico')
+calc.config(background='#FFFFFF')
 
-    display = calc.window['display']
-    display_text = display.DisplayText
+# Adicionando título e visor
+title = tk.Label(calc, text='Calculadora', font=('Bahnschrift', 18), background='#FFFFFF')
+title.grid(row=0, column=0, columnspan=5, pady=8)
 
-    if calc.event not in not_number and len(display_text) < 15:
-        if display_text == '0':
-            display.update(calc.event)
-        if display_text != '0':
-            display.update(f'{display_text}{calc.event}')
+display = tk.Entry(calc, cursor='xterm', width=21, font=('System', 36), justify='right', background='#C3EBEB')
+display.grid(row=1, column=0, columnspan=5, padx=8)
 
-    match calc.event:
-        case 'clear':
-            display.update('0')
-        case 'addition':
-            display.update('+')
-        case 'subtraction':
-            display.update('-')
-        case 'multiplication':
-            display.update('*')
-        case 'division':
-            display.update('/')
-        case 'percent':
-            display.update('%')
-        case 'sq_root':
-            display.update('√')
-        case 'square':
-            display.update('^')
-        case 'equal':
-            display.update('=')
-        case 'comma':
-            if ',' not in display_text:
-                display.update(f'{display_text},')
+# Adicionando os botões
+clear = tk.Button(calc, text='C', width=7, height=2, font=('Terminal', 12),
+                  relief='raised', borderwidth=3, command=change_number)
+clear.grid(row=2, column=0, padx=5, pady=5)
+
+seno = tk.Button(calc, text='sin()', width=7, height=2, font=('Terminal', 12),
+                 relief='raised', borderwidth=3, command=change_number)
+seno.grid(row=2, column=1, padx=5, pady=5)
+
+coss = tk.Button(calc, text='cos()', width=7, height=2, font=('Terminal', 12),
+                 relief='raised', borderwidth=3, command=change_number)
+coss.grid(row=2, column=2, padx=5, pady=5)
+
+tang = tk.Button(calc, text='tan()', width=7, height=2, font=('Terminal', 12),
+                 relief='raised', borderwidth=3, command=change_number)
+tang.grid(row=2, column=3, padx=5, pady=5)
+
+porc = tk.Button(calc, text='%', width=7, height=2, font=('Terminal', 12),
+                 relief='raised', borderwidth=3, command=change_number)
+porc.grid(row=2, column=4, padx=5, pady=5)
+
+square = tk.Button(calc, text='√', width=7, height=2, font=('Terminal', 12),
+                   relief='raised', borderwidth=3, command=change_number)
+square.grid(row=3, column=0, padx=5, pady=5)
+
+open_par = tk.Button(calc, text='(', width=7, height=2, font=('Terminal', 12),
+                     relief='raised', borderwidth=3, command=change_number)
+open_par.grid(row=3, column=1, padx=5, pady=5)
+
+close_par = tk.Button(calc, text=')', width=7, height=2, font=('Terminal', 12),
+                      relief='raised', borderwidth=3, command=change_number)
+close_par.grid(row=3, column=2, padx=5, pady=5)
+
+pi_num = tk.Button(calc, text='π', width=7, height=2, font=('Terminal', 12),
+                   relief='raised', borderwidth=3, command=change_number)
+pi_num.grid(row=3, column=3, padx=5, pady=5)
+
+divide = tk.Button(calc, text='/', width=7, height=2, font=('Terminal', 12),
+                   relief='raised', borderwidth=3, command=change_number)
+divide.grid(row=3, column=4, padx=5, pady=5)
+
+power = tk.Button(calc, text='^', width=7, height=2, font=('Terminal', 12),
+                  relief='raised', borderwidth=3, command=change_number)
+power.grid(row=4, column=0, padx=5, pady=5)
+
+seven = tk.Button(calc, text='7', width=7, height=2, font=('Terminal', 12),
+                  relief='raised', borderwidth=3, command=change_number)
+seven.grid(row=4, column=1, padx=5, pady=5)
+
+eight = tk.Button(calc, text='8', width=7, height=2, font=('Terminal', 12),
+                  relief='raised', borderwidth=3, command=change_number)
+eight.grid(row=4, column=2, padx=5, pady=5)
+
+nine = tk.Button(calc, text='9', width=7, height=2, font=('Terminal', 12),
+                 relief='raised', borderwidth=3, command=change_number)
+nine.grid(row=4, column=3, padx=5, pady=5)
+
+times = tk.Button(calc, text='*', width=7, height=2, font=('Terminal', 12),
+                  relief='raised', borderwidth=3, command=change_number)
+times.grid(row=4, column=4, padx=5, pady=5)
+
+ten_power = tk.Button(calc, text='10ⁿ', width=7, height=2, font=('Terminal', 12),
+                      relief='raised', borderwidth=3, command=change_number)
+ten_power.grid(row=5, column=0, padx=5, pady=5)
+
+four = tk.Button(calc, text='4', width=7, height=2, font=('Terminal', 12),
+                 relief='raised', borderwidth=3, command=change_number)
+four.grid(row=5, column=1, padx=5, pady=5)
+
+five = tk.Button(calc, text='5', width=7, height=2, font=('Terminal', 12),
+                 relief='raised', borderwidth=3, command=change_number)
+five.grid(row=5, column=2, padx=5, pady=5)
+
+six = tk.Button(calc, text='6', width=7, height=2, font=('Terminal', 12),
+                relief='raised', borderwidth=3, command=change_number)
+six.grid(row=5, column=3, padx=5, pady=5)
+
+minus = tk.Button(calc, text='-', width=7, height=2, font=('Terminal', 12),
+                  relief='raised', borderwidth=3, command=change_number)
+minus.grid(row=5, column=4, padx=5, pady=5)
+
+fact = tk.Button(calc, text='n!', width=7, height=2, font=('Terminal', 12),
+                 relief='raised', borderwidth=3, command=change_number)
+fact.grid(row=6, column=0, padx=5, pady=5)
+
+one = tk.Button(calc, text='1', width=7, height=2, font=('Terminal', 12),
+                relief='raised', borderwidth=3, command=change_number)
+one.grid(row=6, column=1, padx=5, pady=5)
+
+two = tk.Button(calc, text='2', width=7, height=2, font=('Terminal', 12),
+                relief='raised', borderwidth=3, command=change_number)
+two.grid(row=6, column=2, padx=5, pady=5)
+
+three = tk.Button(calc, text='3', width=7, height=2, font=('Terminal', 12),
+                  relief='raised', borderwidth=3, command=change_number)
+three.grid(row=6, column=3, padx=5, pady=5)
+
+plus = tk.Button(calc, text='+', width=7, height=2, font=('Terminal', 12),
+                 relief='raised', borderwidth=3, command=change_number)
+plus.grid(row=6, column=4, padx=5, pady=5)
+
+termial = tk.Button(calc, text='n?', width=7, height=2, font=('Terminal', 12),
+                    relief='raised', borderwidth=3, command=change_number)
+termial.grid(row=7, column=0, padx=5, pady=5)
+
+module = tk.Button(calc, text='|x|', width=7, height=2, font=('Terminal', 12),
+                   relief='raised', borderwidth=3, command=change_number)
+module.grid(row=7, column=1, padx=5, pady=5)
+
+zero = tk.Button(calc, text='0', width=7, height=2, font=('Terminal', 12),
+                 relief='raised', borderwidth=3, command=change_number)
+zero.grid(row=7, column=2, padx=5, pady=5)
+
+dot = tk.Button(calc, text='.', width=7, height=2, font=('Terminal', 12),
+                relief='raised', borderwidth=3, command=change_number)
+dot.grid(row=7, column=3, padx=5, pady=5)
+
+equal = tk.Button(calc, text='=', width=7, height=2, font=('Terminal', 12),
+                  relief='raised', borderwidth=3, command=change_number)
+equal.grid(row=7, column=4, padx=5, pady=5)
+
+# Mantendo a janela aberta
+calc.mainloop()
